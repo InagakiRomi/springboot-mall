96389448dfc5d3e7f769ee3fd6e8f12252fe4fd8
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-04-13 13:39:09 +0800

    初始化 Spring Boot project

---
+++ springooot-mall/src/main/java/com/inagakiromi/springooot_mall/SpringoootMallApplication.java
@@ -0,0 +1,13 @@
+package com.inagakiromi.springooot_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringoootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringoootMallApplication.class, args);
+	}
+
+}
---
+++ springooot-mall/src/test/java/com/inagakiromi/springooot_mall/SpringoootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inagakiromi.springooot_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
