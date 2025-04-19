68d3839e4261cc7198240efed3db79b9638f4b25
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-04-19 15:32:57 +0800

    [+]單元測試創建

---
+++ springooot-mall/src/test/java/com/inagakiromi/springooot_mall/controller/ProductControllerTest.java
@@ -0,0 +1,14 @@
+package com.inagakiromi.springooot_mall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
